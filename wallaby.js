fac882663f97e4e9a6543db20757d91f74471613
Christian Wlatschiha <c.wlatschiha@gmail.com>
2020-08-18 23:52:15 +0200

    chore: speedup test execution

--- wallaby.js
+++ wallaby.js
@@ -1,3 +1,36 @@
 module.exports = () => ({
-  autoDetect: true,
+  // debug: true,
+
+  // trace: true,
+
+  files: [
+    'package.json',
+    'tsconfig.json',
+    'src/**/*.ts',
+    '!src/main.ts',
+    '!src/app/app.module.ts',
+    '!src/**/*.spec.ts',
+  ],
+
+  tests: ['src/**/*.spec.ts'],
+
+  env: {
+    type: 'node',
+  },
+
+  testFramework: 'jest',
+
+  setup(wallaby) {
+    const jestConfig = require('./package.json').jest;
+    wallaby.testFramework.configure(jestConfig);
+  },
+
+  preprocessors: {
+    '**/*.js?(x)': (file) =>
+      require('@babel/core').transform(file.content, {
+        sourceMap: true,
+        filename: file.path,
+        presets: [require('babel-preset-jest')],
+      }),
+  },
 });

191f685c167a4572fa61f97f1a4218a9ab8b759b
Christian Wlatschiha <c.wlatschiha@gmail.com>
2020-08-18 23:54:48 +0200

    test(controller): fix failing test

--- src/modules/task/task.controller.spec.ts
+++ src/modules/task/task.controller.spec.ts
@@ -3,10 +3,12 @@ import { mock, mockReset } from 'jest-mock-extended';
 import { TaskController } from './task.controller';
 import { GetAllTasksUseCase } from './use-cases/get-all-tasks/get-all-tasks.use-case';
 import { NoteTaskUseCase } from './use-cases/note-task/note-task.use-case';
+import { TickOffTaskUseCase } from './use-cases/tick-off-task/tick-off-task.use-case';
 
 describe('TaskController', () => {
   const mockedGetAllTasksUseCase = mock<GetAllTasksUseCase>();
   const mockedNoteTaskUseCase = mock<NoteTaskUseCase>();
+  const mockedTickOffTaskUseCase = mock<TickOffTaskUseCase>();
   let controller: TaskController;
 
   beforeEach(async () => {
@@ -15,6 +17,7 @@ describe('TaskController', () => {
       providers: [
         { provide: GetAllTasksUseCase, useValue: mockedGetAllTasksUseCase },
         { provide: NoteTaskUseCase, useValue: mockedNoteTaskUseCase },
+        { provide: TickOffTaskUseCase, useValue: mockedTickOffTaskUseCase },
       ],
     }).compile();
 
@@ -24,6 +27,7 @@ describe('TaskController', () => {
   afterAll(() => {
     mockReset(mockedGetAllTasksUseCase);
     mockReset(mockedNoteTaskUseCase);
+    mockReset(mockedTickOffTaskUseCase);
   });
 
   it('should be defined', () => {

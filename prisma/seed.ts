f0a5533ff42b9bde5adb82784a67e244bb7f8cee
Christian Wlatschiha <c.wlatschiha@gmail.com>
2021-02-09 00:10:02 +0100

    build(db): create multiple users at once

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,41 +10,44 @@ async function hashPassword(password: string): Promise<string> {
 }
 
 async function main(): Promise<void> {
-  await prisma.baseUserModel.create({
-    data: {
-      baseUserId: e2eUser.id,
-      createdAt: new Date().toISOString(),
-      isEmailVerified: e2eUser.isEmailVerified,
-      userEmail: e2eUser.email,
-      username: e2eUser.username,
-      userPassword: await hashPassword(e2eUser.password),
-      memberModel: {
-        create: {
-          createdAt: new Date().toISOString(),
-          memberId: e2eUser.memberId,
-        },
+  const result = await prisma.baseUserModel.createMany({
+    data: [
+      {
+        baseUserId: e2eUser.id,
+        createdAt: new Date().toISOString(),
+        isEmailVerified: e2eUser.isEmailVerified,
+        userEmail: e2eUser.email,
+        username: e2eUser.username,
+        userPassword: await hashPassword(e2eUser.password),
       },
-    },
+      {
+        baseUserId: noMemberUser.id,
+        createdAt: new Date().toISOString(),
+        isEmailVerified: noMemberUser.isEmailVerified,
+        userEmail: noMemberUser.email,
+        username: noMemberUser.username,
+        userPassword: await hashPassword(noMemberUser.password),
+      },
+    ],
   });
+  console.log(`Created ${result.count} users.`);
 
-  await prisma.baseUserModel.create({
+  await prisma.memberModel.create({
     data: {
-      baseUserId: noMemberUser.id,
       createdAt: new Date().toISOString(),
-      isEmailVerified: noMemberUser.isEmailVerified,
-      userEmail: noMemberUser.email,
-      username: noMemberUser.username,
-      userPassword: await hashPassword(noMemberUser.password),
+      memberBaseId: e2eUser.id,
+      memberId: e2eUser.memberId,
     },
   });
+  console.log(`Created associated member for user ${e2eUser.username}.`);
 }
 
 main()
   .catch((error) => {
-    // eslint-disable-next-line no-console
     console.error(error);
     process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
+    console.log('Disconnected from database.');
   });

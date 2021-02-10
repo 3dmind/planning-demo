504e6b6b95f40cc60ec24701498cd3f898f98e27
Christian Wlatschiha <c.wlatschiha@gmail.com>
2021-02-10 12:09:57 +0100

    test: improve code coverage

--- src/domain-event-publisher/domain-event-publisher.service.spec.ts
+++ src/domain-event-publisher/domain-event-publisher.service.spec.ts
@@ -1,8 +1,27 @@
-import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AggregateRoot, DomainEvent, UniqueEntityId } from '../shared/domain';
 import { DomainEventPublisherService } from './domain-event-publisher.service';
 
 describe('DomainEventPublisherService', () => {
+  class DomainEventMock implements DomainEvent {
+    readonly occurredOn: Date;
+    readonly entity: EntityMock;
+
+    constructor(entity: EntityMock) {
+      this.occurredOn = new Date();
+      this.entity = entity;
+    }
+  }
+
+  class EntityMock extends AggregateRoot<null> {
+    constructor(props: null, id: UniqueEntityId) {
+      super(props, id);
+      this.addDomainEvent(new DomainEventMock(this));
+    }
+  }
+
+  let eventEmitter: EventEmitter2;
   let service: DomainEventPublisherService;
 
   beforeAll(async () => {
@@ -10,7 +29,9 @@ describe('DomainEventPublisherService', () => {
       imports: [EventEmitterModule.forRoot({})],
       providers: [DomainEventPublisherService],
     }).compile();
+    module.useLogger(false);
 
+    eventEmitter = module.get<EventEmitter2>(EventEmitter2);
     service = module.get<DomainEventPublisherService>(
       DomainEventPublisherService,
     );
@@ -18,5 +39,17 @@ describe('DomainEventPublisherService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should publish domain events', () => {
+    expect.assertions(1);
+    const entityId = new UniqueEntityId();
+    const entityMock = new EntityMock(null, entityId);
+    const listenerSpy = jest.fn();
+    eventEmitter.on(DomainEventMock.name, listenerSpy);
+
+    service.publish(entityMock);
+
+    expect(listenerSpy).toHaveBeenCalledTimes(1);
   });
 });

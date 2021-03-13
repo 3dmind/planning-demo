cd8b969acc32e8efaca73fe021cc37a8799ae03f
Christian Wlatschiha <c.wlatschiha@gmail.com>
2021-03-13 20:09:37 +0100

    refactor: access domain events PLD-56

--- src/shared/domain/events/domain-event.collection.ts
+++ src/shared/domain/events/domain-event.collection.ts
@@ -11,8 +11,6 @@ export class DomainEventCollection {
     this.domainEvents.length = 0;
   }
 
-  // Refactor: Combine `toArray` and `clear` in one method
-  // See https://github.com/3dmind/java-ddd-skeleton/blob/38383e48f8f893561f48c89f61639b3ed846d5b5/src/shared/main/tv/codely/shared/domain/AggregateRoot.java#L12
   public toArray(): DomainEvent[] {
     return [...this.domainEvents];
   }

2f960e7fb566fcd5b8126e4ed04f6c0c2c1e1f02
Christian Wlatschiha <c.wlatschiha@gmail.com>
2021-05-25 22:09:09 +0200

    fix(sonar-qube): member is never reassigned; mark it as readonly

--- src/shared/core/or-specification.ts
+++ src/shared/core/or-specification.ts
@@ -2,8 +2,8 @@ import { Specification } from './specification.interface';
 
 export class OrSpecification<E> implements Specification<E> {
   constructor(
-    private first: Specification<E>,
-    private second: Specification<E>,
+    private readonly first: Specification<E>,
+    private readonly second: Specification<E>,
   ) {}
 
   public satisfiedBy(entity: E): boolean {
